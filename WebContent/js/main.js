c722d7cf9d1b3b510d50e9e88db5ac5c310c60f5
Wayne Jiang <jnoodle@163.com>
2014-10-17 16:45:54 +0800

    修改IE兼容样式

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -8,18 +8,28 @@ var context = '/npc';
 
 $(document).ready(function () {
 
+    if (window.PIE) {
+        $('.c3').each(function () {
+            PIE.attach(this);
+        });
+    }
+
     //首页届别浮动文字
     $("#session-nav li a").on("mouseenter", function () {
         $(this).find("span").show();
+        $(this).find("img").hide();
     }).on("mouseleave", function () {
         $(this).find("span").hide();
+        $(this).find("img").show();
     });
 
     //二级页面全部图片浮动文字
     $("#gallery-content li a").on("mouseenter", function () {
-        $(this).find("span").show();
+        var t = $(this);
+        t.find("span").width(t.width()).height(t.height()).show();
     }).on("mouseleave", function () {
-        $(this).find("span").hide();
+        var t = $(this);
+        t.find("span").hide();
     });
 
     //子页面相关信息显示隐藏
@@ -36,9 +46,9 @@ $(document).ready(function () {
         var materialType = source.prop('class');
         if (materialType == 'video') {
             //http://mediaelementjs.com/
-            var _src = context + "/" + source.attr('file');
-            var player = $('<video src="' + _src + '" width="720" height="576" controls="controls" preload="none">' +
-                '<source type="video/flv" src="' + context + "/" + source.attr('file') + '" />' +
+            var _src = (context + "/" + source.attr('file'));
+            var player = $('<video id="video" src="' + _src + '" width="720" height="576" controls="controls" preload="none">' +
+                '<source type="video/flv" src="' + _src + '" />' +
                 '<object width="720" height="576" type="application/x-shockwave-flash" data="' + context + '/js/vendor/flashmediaelement.swf">' +
                 '<param name="movie" value="' + context + '/js/vendor/flashmediaelement.swf" />' +
                 '<param name="flashvars" value="controls=true&file=' + _src + '" />' +
@@ -76,9 +86,9 @@ $(document).ready(function () {
         $("#gallery-content").hide();
     });
 
-    $("footer .gotop").click(function (e) {
+    $("#gotop").click(function (e) {
         e.preventDefault();
-        $('body').animate({scrollTop: '0px'}, 500);
+        $('body, html').animate({scrollTop: '0px'}, 500);
     });
 });
 
